85f2f778d1c8b75bc150bbdc34e8405d6e7b1a1e
Maxmitab <maxpavlenko@hotmail.com>
2018-02-08 00:17:38 +0300

    lesson5 update

--- lesson_4/DZ_task_2.js
+++ lesson_4/DZ_task_2.js
@@ -7,8 +7,14 @@ checkSpam('New PSD template'); // false
  */
 
 function checkSpam(str) {
+    var lowerStr = str.toLowerCase();
+    return !!(~lowerStr.indexOf('sex') || ~lowerStr.indexOf('spam'));
+}
+
+// вариант деревянный
+function checkSpam2(str) {
     return (str.indexOf('spam') > 0 || str.indexOf('SPAM') > 0 || str.indexOf('Spam') > 0) ? console.log(true) :
         (str.indexOf('sex') > 0 || str.indexOf('SEX') > 0 || str.indexOf('Sex') > 0) ? console.log(true) : console.log(false);
 }
 
-checkSpam('[SPAM] How to earn fast money?');
+console.log(checkSpam('[SEX] How to earn fast money?'));
